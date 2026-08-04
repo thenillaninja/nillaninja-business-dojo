e94f6b061193326bffa7a5d44bb559f37943874a
Mark Evans <thenillaninja@gmail.com>
2026-08-04 02:04:35 -0400

    Add file organization recommendation rule

--- data/recommendation-rules.js
+++ data/recommendation-rules.js
@@ -38,5 +38,13 @@ export const recommendationRules = [
     questionId: "technology-duplicate-data-entry",
     maximumAnswerValue: 0.67,
     priorityOverride: null
+  },
+  {
+    id: "rule-organize-business-files",
+    recommendationId: "organize-business-files",
+    triggerType: "answer-threshold",
+    questionId: "technology-file-organization",
+    maximumAnswerValue: 0.67,
+    priorityOverride: null
   }
 ];
