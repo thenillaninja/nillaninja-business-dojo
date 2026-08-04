68e0e518a99a88e20698f106f16f7a8247cc0a29
Mark Evans <thenillaninja@gmail.com>
2026-08-04 02:00:35 -0400

    Add written procedures recommendation rule

---
+++ data/recommendation-rules.js
@@ -0,0 +1,10 @@
+export const recommendationRules = [
+  {
+    id: "rule-document-recurring-processes",
+    recommendationId: "document-recurring-processes",
+    triggerType: "answer-threshold",
+    questionId: "operations-written-procedures",
+    maximumAnswerValue: 0.67,
+    priorityOverride: null
+  }
+];
